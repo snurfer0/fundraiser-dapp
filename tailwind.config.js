667d4246e7201987ce7d2f54521103e22af9cbc0
Andrei Balanuta <43002968+snurfer0@users.noreply.github.com>
2023-12-08 20:52:58 +0200

    fix: add safelist for button dynamic styles

--- tailwind.config.js
+++ tailwind.config.js
@@ -13,7 +13,7 @@ module.exports = {
         prefix: "", // prefix for daisyUI classnames (components, modifiers and responsive class names. Not colors)
         logs: true, // Shows info about daisyUI version and used config in the console when building your CSS
         themeRoot: ":root", // The element that receives theme color CSS variables
-      },
-
+    },
     plugins: [require("daisyui")],
-  }
+    content: ["./src/styles/safelist.txt"],
+}
