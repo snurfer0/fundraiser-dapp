9c4faca03c5e321db577718548adeff8f142b648
Andrei Balanuta <43002968+snurfer0@users.noreply.github.com>
2023-12-09 00:19:31 +0200

    fix: dialog

--- src/components/molecules/modal.tsx
+++ src/components/molecules/modal.tsx
@@ -13,24 +13,22 @@ interface Props {
 const Modal = forwardRef<HTMLDialogElement, Props>(
   ({ id, title, description, children, actions, onClose }, ref) => {
     return (
-      <>
-        <dialog id={id} className="modal" ref={ref} onClose={onClose}>
-          <div className="modal-box">
-            <form method="dialog">
-              <button className="btn btn-sm btn-circle btn-ghost absolute right-2 top-2">
-                ✕
-              </button>
-            </form>
-            {title && <h3 className="font-bold text-lg">{title}</h3>}
-            {description && <div className="py-4">{description}</div>}
-            <div className="py-4">{children}</div>
-            <div className="modal-action">{actions}</div>
-          </div>
-          <form method="dialog" className="modal-backdrop">
-            <button>close</button>
+      <dialog id={id} className="modal" ref={ref} onClose={onClose}>
+        <div className="modal-box">
+          <form method="dialog">
+            <button className="btn btn-sm btn-circle btn-ghost absolute right-2 top-2">
+              ✕
+            </button>
           </form>
-        </dialog>
-      </>
+          {title && <h3 className="font-bold text-lg">{title}</h3>}
+          {description && <div className="py-4">{description}</div>}
+          <div className="py-4">{children}</div>
+          <div className="modal-action">{actions}</div>
+        </div>
+        <form method="dialog" className="modal-backdrop">
+          <button>close</button>
+        </form>
+      </dialog>
     );
   }
 );
