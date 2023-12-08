603718373c9c1df86f8d4e2a89803e17b26d1282
Andrei Balanuta <43002968+snurfer0@users.noreply.github.com>
2023-12-08 21:35:52 +0200

    fix: themes

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,8 +5,8 @@ module.exports = {
     "./src/components/**/*.{js,ts,jsx,tsx}",
   ],
   daisyui: {
-    themes: ["light", "dark"],
-    darkTheme: "dark", // name of one of the included themes for dark mode
+    themes: ["pastel", "forest"],
+    darkTheme: "forest", // name of one of the included themes for dark mode
     base: true, // applies background color and foreground color for root element by default
     styled: true, // include daisyUI colors and design decisions for all components
     utils: true, // adds responsive and modifier utility classes
