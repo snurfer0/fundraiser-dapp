29a6804d911935d1245cc42d15f012f52ce305c3
Andrei Balanuta <43002968+snurfer0@users.noreply.github.com>
2023-12-09 19:01:23 +0200

    feat: wallet connect button with account modal

--- src/components/molecules/wallet.connect.button.tsx
+++ src/components/molecules/wallet.connect.button.tsx
@@ -2,18 +2,72 @@
 
 import React from 'react';
 import { CreditCard } from 'react-feather';
-import { useWalletContext } from 'src/contexts/wallet.context';
+import {
+  useAccount,
+  useBalance,
+  useConnect,
+  useDisconnect,
+  useEnsAvatar,
+  useEnsName,
+  useNetwork,
+} from 'wagmi';
 import { ButtonVariant } from 'src/enums/button.variant.enum';
 import { shortenEthereumAddress } from 'src/helpers/short.address';
 import { Button } from '../atoms';
+import { Account } from '../organisms';
+import Modal from './modal';
 
 const WalletConnectButton = () => {
-  const { account, connectToWallet } = useWalletContext();
+  const { address, isConnected } = useAccount();
+  const { data } = useBalance({ address });
+  const { connect, connectors } = useConnect();
+  const { data: ensName } = useEnsName({ address });
+  const { data: ensAvatar } = useEnsAvatar({ name: ensName });
+  const { disconnect } = useDisconnect();
+  const { chain } = useNetwork();
+  const accountModalRef = React.useRef<HTMLDialogElement>(null);
+
+  function onDisconnect() {
+    const accountModal = accountModalRef.current;
+    if (accountModal && accountModal.close) {
+      accountModal.close();
+    }
+    disconnect();
+  }
+
+  function showAccountModal() {
+    const accountModal = accountModalRef.current;
+    if (accountModal && accountModal.showModal) {
+      accountModal.showModal();
+    }
+  }
+
+  function onClick() {
+    if (isConnected) {
+      return showAccountModal();
+    }
+    void connect({ connector: connectors[0] });
+  }
+
   return (
-    <Button onClick={connectToWallet} variant={ButtonVariant.Secondary}>
-      <CreditCard />
-      <span>{account ? shortenEthereumAddress(account) : 'Connect'}</span>
-    </Button>
+    <>
+      <Button onClick={onClick} variant={ButtonVariant.Secondary}>
+        <CreditCard />
+        <span>{address ? shortenEthereumAddress(address) : 'Connect'}</span>
+      </Button>
+      {address && isConnected && (
+        <Modal id="account" title="Account" ref={accountModalRef}>
+          <Account
+            address={address}
+            balance={Number(data?.formatted)}
+            ensAvatar={ensAvatar}
+            networkId={chain?.id}
+            networkName={chain?.name}
+            onDisconnect={onDisconnect}
+          />
+        </Modal>
+      )}
+    </>
   );
 };
 
